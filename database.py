603b34095a739b9f80b49b92995b3c8b38194bed
b6132766705-coder <b6132766705@gmail.com>
2026-04-15 09:16:10 -0400

    Create database.py

---
+++ database.py
@@ -0,0 +1,23 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import Column, BigInteger, Integer
+
+# Берем ссылку на базу от Railway
+DATABASE_URL = os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+asyncpg://")
+
+engine = create_async_engine(DATABASE_URL)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+    tg_id = Column(BigInteger, primary_key=True)
+    balance = Column(Integer, default=1000)
+    wins = Column(Integer, default=0)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
